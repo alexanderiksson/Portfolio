6a5db183b407b63c8b10d0995f4e25a60434a790
alexanderiksson <Alexerik69@gmail.com>
2024-11-24 22:55:47 +0100

    Added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Layout } from './layout/Layout'
+import Home from './pages/home'
+
 
 function App() {
+    return(
+        <Router>
+            <Routes>
+                <Route element={<Layout/>}>
 
-    return (
-        <></>
+                    <Route path='/' element={<Home/>} />
+
+                </Route>
+            </Routes>
+        </Router>
     )
 }
 

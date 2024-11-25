a4cce0f02e18c8e271f7dfd0901425dd9d5b7701
alexanderiksson <Alexerik69@gmail.com>
2024-11-25 12:33:34 +0100

    Added content to skills page

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -9,7 +9,55 @@ export default function Skills() {
             </Helmet>
 
             <div className="content">
+                <div className="w-full h-screen flex flex-col justify-center items-center">
 
+                    {/* Row 1 */}
+                    <div className="w-full flex justify-evenly mb-8">
+                        <div className="flex flex-col items-center w-1/4">
+                            <img src="html.svg" alt="HTML" width="50" height="50" />
+                            <p>HTML</p>
+                        </div>
+
+                        <div className="flex flex-col items-center w-1/4">
+                            <img src="css.svg" alt="CSS" width="50" height="50" />
+                            <p>CSS</p>
+                        </div>
+
+                        <div className="flex flex-col items-center w-1/4">
+                            <img src="js.svg" alt="JavaScript" width="50" height="50" />
+                            <p>JavaScript</p>
+                        </div>
+
+                        <div className="flex flex-col items-center w-1/4">
+                            <img src="react.svg" alt="React" width="50" height="50" />
+                            <p className="mt-1">React</p>
+                        </div>
+                    </div>
+
+                    {/* Row 2 */}
+                    <div className="w-full flex justify-evenly mt-8">
+                        <div className="flex flex-col items-center w-1/4">
+                            <img src="vue.svg" alt="Vue" width="50" height="50" />
+                            <p>Vue</p>
+                        </div>
+
+                        <div className="flex flex-col items-center w-1/4">
+                            <img src="tailwind.svg" alt="Tailwind" width="50" height="50" />
+                            <p>Tailwind</p>
+                        </div>
+
+                        <div className="flex flex-col items-center w-1/4">
+                            <img src="node.svg" alt="Node.js" width="50" height="50" />
+                            <p>Node.js</p>
+                        </div>
+
+                        <div className="flex flex-col items-center w-1/4">
+                            <img src="php.svg" alt="PHP" width="50" height="50" />
+                            <p>PHP</p>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </React.Fragment>
     )

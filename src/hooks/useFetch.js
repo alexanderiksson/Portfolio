eb05a562b2ec1eca3c6258c343e78a24777dbac7
alexanderiksson <Alexerik69@gmail.com>
2024-11-25 12:33:02 +0100

    Created hook for fetching data

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+
+        fetch(url, {
+            mode: "no-cors",
+            headers: {'Content-Type': 'application/json'}
+        })
+        .then(response => {
+            if (!response) {
+                throw new Error(response.status)
+            }
+            return response.json()
+        })
+        .then(data => {
+            setData(data)
+            setLoading(false)
+        })
+        .catch(err => {
+            setError(err)
+            setLoading(false)
+        })
+
+    }, [url])
+
+    return { data, loading, error }
+
+}
+
+export default useFetch

51bed5e045b3c924d7854e7782d5b0f5b9724c62
alexanderiksson <Alexerik69@gmail.com>
2024-11-25 11:04:49 +0100

    Added active states to nav links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
     return(
         <header className="flex justify-center py-8 fixed w-full">
             <nav>
                 <ul>
-                    <li className="inline mx-8"><Link to="/">Home</Link></li>
-                    <li className="inline mx-8"><Link to="/skills">Skills</Link></li>
-                    <li className="inline mx-8"><Link to="/portfolio">Portfolio</Link></li>
-                    <li className="inline mx-8"><Link to="/contact">Contact</Link></li>
+                    <li className="inline mx-8"><NavLink to="/">Home</NavLink></li>
+                    <li className="inline mx-8"><NavLink to="/skills">Skills</NavLink></li>
+                    <li className="inline mx-8"><NavLink to="/portfolio">Portfolio</NavLink></li>
+                    <li className="inline mx-8"><NavLink to="/contact">Contact</NavLink></li>
                 </ul>
             </nav>
         </header>

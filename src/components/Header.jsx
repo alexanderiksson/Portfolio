e93a5f3850e63e59f52e837c64a530f4859bd4b0
alexanderiksson <Alexerik69@gmail.com>
2024-11-26 13:37:28 +0100

    Updated header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,62 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+const Navbar = () => {
+    return(
+        <nav className="hidden sm:block">
+            <ul className="flex justify-center">
+                <li className="mx-4 sm:mx-8">
+                    <NavLink to="/">Home</NavLink>
+                </li>
+                <li className="mx-4 sm:mx-8">
+                    <NavLink to="/skills">Skills</NavLink>
+                </li>
+                <li className="mx-4 sm:mx-8">
+                    <NavLink to="/portfolio">Portfolio</NavLink>
+                </li>
+                <li className="mx-4 sm:mx-8">
+                    <NavLink to="/contact">Contact</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+};
 
 export default function Header() {
 
     const [isOpen, setIsOpen] = useState(false)
-
     const toggleMenu = () => {
         setIsOpen((open) => !open)
     }
 
+    const [scrolled, setScrolled] = useState(false)
+    useEffect(() => {
+
+        const handleScroll = () => {
+          window.scrollY > 50 ? setScrolled(true) : setScrolled(false)
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => {
+          window.removeEventListener('scroll', handleScroll)
+        }
+      }, [])
+
+
     return(
         <React.Fragment>
-            <header className="flex justify-end sm:justify-center py-8 fixed w-full">
-                <nav className="hidden sm:block">
-                    <ul>
-                        <li className="inline mx-4 sm:mx-8"><NavLink to="/">Home</NavLink></li>
-                        <li className="inline mx-4 sm:mx-8"><NavLink to="/skills">Skills</NavLink></li>
-                        <li className="inline mx-4 sm:mx-8"><NavLink to="/portfolio">Portfolio</NavLink></li>
-                        <li className="inline mx-4 sm:mx-8"><NavLink to="/contact">Contact</NavLink></li>
-                    </ul>
-                </nav>
+            <header className={`flex justify-end sm:justify-center py-4 sm:py-6 fixed w-full ${scrolled ? "scrolled" : ""}`}>
+                <Navbar />
+
+                {/* Hamburger menu */}
                 <div className="block sm:hidden mr-8">
                     <img src="/hamburger.svg" alt="" width="40" onClick={toggleMenu} className="cursor-pointer" />
                 </div>
             </header>
 
-            {/* Dropdown */}
+            {/* Mobile nav */}
             <div id="dropdown" className={`fixed h-screen w-48 px-8 py-6 bg-slate-900 ${isOpen ? "open" : ""}`}>
                 <img src="/close.svg" alt="" width="25" onClick={toggleMenu} className="cursor-pointer" />
                 <nav className="mt-8">

fbd338b095914aabf498a2c73c8974001f45c186
alexanderiksson <Alexerik69@gmail.com>
2024-11-26 15:28:23 +0100

    Added contact icons

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,7 +9,9 @@ export default function Contact() {
             </Helmet>
 
             <div className="content">
-
+                <img src="/email.svg" alt="" width="40px"/>
+                <img src="/linkedin.svg" alt="" width="40px" />
+                <img src="/github.svg" alt="" width="40px" />
             </div>
         </React.Fragment>
     )

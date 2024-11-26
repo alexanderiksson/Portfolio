f07b81313fbfe1a778f8155f48e98b069ad0369b
alexanderiksson <Alexerik69@gmail.com>
2024-11-26 16:29:45 +0100

    Added contact links

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,9 +9,22 @@ export default function Contact() {
             </Helmet>
 
             <div className="content">
-                <img src="/email.svg" alt="" width="40px"/>
-                <img src="/linkedin.svg" alt="" width="40px" />
-                <img src="/github.svg" alt="" width="40px" />
+                <div className="w-full flex flex-col justify-center items-center">
+                    <div className="flex flex-col gap-4">
+                        <div className="flex items-center gap-2">
+                            <img src="/email.svg" alt="" width="40px"/>
+                            <a className="text-white" href="mailto:hello@alexeriksson.se">hello@alexeriksson.se</a>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <img src="/linkedin.svg" alt="" width="40px"/>
+                            <a className="text-white" href="https://www.linkedin.com/in/alexander-eriksson-302bb8237/">LinkedIn</a>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <img src="/github.svg" alt="" width="40px"/>
+                            <a className="text-white" href="https://github.com/alexanderiksson">Github</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </React.Fragment>
     )

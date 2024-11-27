5204386079ce62b159c1a5718ff9ed2774282f9a
alexanderiksson <Alexerik69@gmail.com>
2024-11-27 11:41:23 +0100

    Added new icons

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -15,22 +15,22 @@ export default function Skills() {
                     <div className="w-full flex flex-col sm:flex-row justify-center items-center gap-6">
                         <div className="flex flex-col items-center w-1/4">
                             <img src="html.svg" alt="HTML" width="50" height="50" />
-                            <p className="font-semibold">HTML</p>
+                            <p className="mt-4 font-semibold">HTML</p>
                         </div>
 
                         <div className="flex flex-col items-center w-1/4">
                             <img src="css.svg" alt="CSS" width="50" height="50" />
-                            <p className="font-semibold">CSS</p>
+                            <p className="mt-4 font-semibold">CSS</p>
                         </div>
 
                         <div className="flex flex-col items-center w-1/4">
                             <img src="js.svg" alt="JavaScript" width="50" height="50" />
-                            <p className="font-semibold">JavaScript</p>
+                            <p className="mt-4 font-semibold">JavaScript</p>
                         </div>
 
                         <div className="flex flex-col items-center w-1/4">
                             <img src="react.svg" alt="React" width="50" height="50" />
-                            <p className="mt-1 font-semibold">React</p>
+                            <p className="mt-4 font-semibold">React</p>
                         </div>
                     </div>
 
@@ -38,22 +38,22 @@ export default function Skills() {
                     <div className="w-full flex flex-col sm:flex-row justify-center items-center gap-6">
                         <div className="flex flex-col items-center w-1/4">
                             <img src="vue.svg" alt="Vue" width="50" height="50" />
-                            <p className="font-semibold">Vue</p>
+                            <p className="mt-4 font-semibold">Vue</p>
                         </div>
 
                         <div className="flex flex-col items-center w-1/4">
                             <img src="tailwind.svg" alt="Tailwind" width="50" height="50" />
-                            <p className="font-semibold">Tailwind</p>
+                            <p className="mt-4 font-semibold">Tailwind</p>
                         </div>
 
                         <div className="flex flex-col items-center w-1/4">
                             <img src="node.svg" alt="Node.js" width="50" height="50" />
-                            <p className="font-semibold">Node.js</p>
+                            <p className="mt-4 font-semibold">Node.js</p>
                         </div>
 
                         <div className="flex flex-col items-center w-1/4">
                             <img src="php.svg" alt="PHP" width="50" height="50" />
-                            <p className="font-semibold">PHP</p>
+                            <p className="mt-4 font-semibold">PHP</p>
                         </div>
                     </div>
 

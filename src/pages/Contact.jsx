c276f9aca2aeda18773b5b9dea697b44b43b2116
alexanderiksson <Alexerik69@gmail.com>
2024-11-27 16:28:37 +0100

    Changed contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,16 +13,15 @@ export default function Contact() {
                     <p className="max-w-md text-center mb-12">If you have any questions or want to know more about me, don't hesitate to reach out.</p>
                     <div className="flex flex-col gap-4">
                         <div className="flex items-center gap-4">
-                            <img src="/mail.svg" alt="" width="32px"/>
                             <a className="text-white" href="mailto:hello@alexeriksson.se">hello@alexeriksson.se</a>
                         </div>
-                        <div className="flex items-center gap-4">
-                            <img src="/linkedin.svg" alt="" width="32px"/>
-                            <a className="text-white" href="https://www.linkedin.com/in/alexander-eriksson-302bb8237/">LinkedIn</a>
-                        </div>
-                        <div className="flex items-center gap-4">
-                            <img src="/github.svg" alt="" width="32px"/>
-                            <a className="text-white" href="https://github.com/alexanderiksson">Github</a>
+                        <div className="flex justify-center items-center gap-4">
+                            <a className="text-white" href="https://www.linkedin.com/in/alexander-eriksson-302bb8237/">
+                                <img src="/linkedin.svg" alt="" width="40px"/>
+                            </a>
+                            <a className="text-white" href="https://github.com/alexanderiksson">
+                                <img src="/github.svg" alt="" width="32px"/>
+                            </a>
                         </div>
                     </div>
                 </div>

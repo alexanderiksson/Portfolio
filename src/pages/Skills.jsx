c2e05f5b231f9876c50c544d6483a74d16b25c90
alexanderiksson <Alexerik69@gmail.com>
2024-11-27 15:00:03 +0100

    Added progress bar to skills page

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -13,47 +13,71 @@ export default function Skills() {
 
                     {/* Row 1 */}
                     <div className="w-full flex flex-col sm:flex-row justify-center items-center gap-6">
-                        <div className="flex flex-col items-center w-1/4">
+                        <div className="flex flex-col items-center w-full sm:w-1/4 gap-4">
                             <img src="html.svg" alt="HTML" width="50" height="50" />
-                            <p className="mt-4 font-semibold">HTML</p>
+                            <p className="font-semibold">HTML</p>
+                            <div className="w-3/4 bg-slate-200 h-1">
+                                <div className="progress-bar w-full h-full"></div>
+                            </div>
                         </div>
 
-                        <div className="flex flex-col items-center w-1/4">
+                        <div className="flex flex-col items-center w-full sm:w-1/4 gap-4">
                             <img src="css.svg" alt="CSS" width="50" height="50" />
-                            <p className="mt-4 font-semibold">CSS</p>
+                            <p className="font-semibold">CSS</p>
+                            <div className="w-3/4 bg-slate-200 h-1">
+                                <div className="progress-bar w-11/12 h-full"></div>
+                            </div>
                         </div>
 
-                        <div className="flex flex-col items-center w-1/4">
+                        <div className="flex flex-col items-center w-full sm:w-1/4 gap-4">
                             <img src="js.svg" alt="JavaScript" width="50" height="50" />
-                            <p className="mt-4 font-semibold">JavaScript</p>
+                            <p className="font-semibold">JavaScript</p>
+                            <div className="w-3/4 bg-slate-200 h-1">
+                                <div className="progress-bar w-2/3 h-full"></div>
+                            </div>
                         </div>
 
-                        <div className="flex flex-col items-center w-1/4">
+                        <div className="flex flex-col items-center w-full sm:w-1/4 gap-4">
                             <img src="react.svg" alt="React" width="50" height="50" />
-                            <p className="mt-4 font-semibold">React</p>
+                            <p className="font-semibold">React</p>
+                            <div className="w-3/4 bg-slate-200 h-1">
+                                <div className="progress-bar w-1/2 h-full"></div>
+                            </div>
                         </div>
                     </div>
 
                     {/* Row 2 */}
                     <div className="w-full flex flex-col sm:flex-row justify-center items-center gap-6">
-                        <div className="flex flex-col items-center w-1/4">
+                        <div className="flex flex-col items-center w-full sm:w-1/4 gap-4">
                             <img src="vue.svg" alt="Vue" width="50" height="50" />
-                            <p className="mt-4 font-semibold">Vue</p>
+                            <p className="font-semibold">Vue</p>
+                            <div className="w-3/4 bg-slate-200 h-1">
+                                <div className="progress-bar w-1/3 h-full"></div>
+                            </div>
                         </div>
 
-                        <div className="flex flex-col items-center w-1/4">
+                        <div className="flex flex-col items-center w-full sm:w-1/4 gap-4">
                             <img src="tailwind.svg" alt="Tailwind" width="50" height="50" />
-                            <p className="mt-4 font-semibold">Tailwind</p>
+                            <p className="font-semibold">Tailwind</p>
+                            <div className="w-3/4 bg-slate-200 h-1">
+                                <div className="progress-bar w-3/4 h-full"></div>
+                            </div>
                         </div>
 
-                        <div className="flex flex-col items-center w-1/4">
+                        <div className="flex flex-col items-center w-full sm:w-1/4 gap-4">
                             <img src="node.svg" alt="Node.js" width="50" height="50" />
-                            <p className="mt-4 font-semibold">Node.js</p>
+                            <p className="font-semibold">Node.js</p>
+                            <div className="w-3/4 bg-slate-200 h-1">
+                                <div className="progress-bar w-1/2 h-full"></div>
+                            </div>
                         </div>
 
-                        <div className="flex flex-col items-center w-1/4">
+                        <div className="flex flex-col items-center w-full sm:w-1/4 gap-4">
                             <img src="php.svg" alt="PHP" width="50" height="50" />
-                            <p className="mt-4 font-semibold">PHP</p>
+                            <p className="font-semibold">PHP</p>
+                            <div className="w-3/4 bg-slate-200 h-1">
+                                <div className="progress-bar w-7/12 h-full"></div>
+                            </div>
                         </div>
                     </div>
 

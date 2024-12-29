1f6fff25df0b1beaa0e8e7eb930f28dba182b0ae
alexanderiksson <Alexerik69@gmail.com>
2024-12-29 21:38:27 +0100

    Added contentful host env variable

--- src/hooks/contentfulClient.js
+++ src/hooks/contentfulClient.js
@@ -2,7 +2,8 @@ import { createClient } from 'contentful'
 
 const client = createClient({
   space: import.meta.env.VITE_CONTENTFUL_SPACE_ID,
-  accessToken: import.meta.env.VITE_CONTENTFUL_AUTH_TOKEN
+  accessToken: import.meta.env.VITE_CONTENTFUL_AUTH_TOKEN,
+  host: import.meta.env.VITE_CONTENTFUL_HOST
 });
 
 export default client

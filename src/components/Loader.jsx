54808684e652af005a92febe2ff33902dba28e5e
alexanderiksson <Alexerik69@gmail.com>
2024-12-30 18:08:07 +0100

    Changed loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 
 const Loader = () => {
     return(
-        <div className="loader"></div>
+        <div className="loader">
+            <div class="dot-spinner">
+                <div class="dot-spinner__dot"></div>
+                <div class="dot-spinner__dot"></div>
+                <div class="dot-spinner__dot"></div>
+                <div class="dot-spinner__dot"></div>
+                <div class="dot-spinner__dot"></div>
+                <div class="dot-spinner__dot"></div>
+                <div class="dot-spinner__dot"></div>
+                <div class="dot-spinner__dot"></div>
+            </div>
+        </div>
     )
 }
 
